a4b2dad0423ba0b4dfc949ec55136be4282f31de
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2025-05-22 21:28:36 +0530

    reducer start

---
+++ client/src/api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API =  axios.create({
+    baseURL: 'http://localhost:8000/api/',
+})
+
+export const UserSignUp =  async (data) =>API.post('/user/signup',data)
+
+export const UserSignIn =  async (data) =>API.post('/user/signin',data)
+
+export const getDashboardDetails = async (token) => {
+    API.get('/user/dashboard',{
+        headers :{Authorization: `Bearer ${token}`}
+    })
+}
+export const getWorkouts = async (token,date) => {
+    API.get(`/user/workout${date}`,{
+        headers :{Authorization: `Bearer ${token}`}
+    })
+}
+export const addWorkouts = async (token,data) => {
+    API.get(`/user/workout`,data,{
+        headers :{Authorization: `Bearer ${token}`}
+    })
+}
